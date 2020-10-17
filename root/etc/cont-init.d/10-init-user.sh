24832cfe07182217bcdef1b31b2c8544cc7148f0
h2-heo <h0h6h2h5@gmail.com>
2020-10-17 14:38:52 +0900

    [#12] Resolve home permission issue

--- root/etc/cont-init.d/10-init-user.sh
+++ root/etc/cont-init.d/10-init-user.sh
@@ -5,8 +5,20 @@ if [ ${PUID} -eq 0 ]; then
 fi
 
 groupadd -g ${PGID} ${GROUP_NAME}
-useradd -m -s /usr/bin/userbash -g ${GROUP_NAME} -u ${PUID} ${USER_NAME}
+useradd -md ${USER_HOME} -s /usr/bin/userbash -g ${GROUP_NAME} -u ${PUID} ${USER_NAME}
 usermod -a -G sudo ${USER_NAME}
+
+# When not copied skel, manually copy
+if [ -d "${USER_HOME}" ] && [ "$(stat -c "%U" "${USER_HOME}")" != "${USER_NAME}" ]; then
+  chown ${PUID}:${PGID} ${USER_HOME}
+
+  tmp=/tmp/devcontainer
+  mkdir -p ${tmp}/skel
+  cp -r -T /etc/skel ${tmp}/skel
+  chown -R ${PUID}:${PGID} ${tmp}/skel
+  cp -r -T ${tmp}/skel ${USER_HOME}
+  rm -rf ${tmp}
+fi
 
 # Set default password (= username)
 echo "${USER_NAME}:${USER_NAME}" | chpasswd
